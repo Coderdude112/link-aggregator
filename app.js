12713039a62aa665d7b307f4a737e8cbfb41d56b
IsadoraTerz <84795870+IsadoraTerz@users.noreply.github.com>
2022-05-12 12:43:43 -0600

    previous code uploaded

--- app.js
+++ app.js
@@ -23,4 +23,25 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // --------- //
 app.get("/", (req, res) => { res.sendFile(__dirname + "\\web_root\\index.html") }) // Set the default homepage
 
+app.get('/gimme', async (req, res) => {
+  var name = (await dao.getAllMemes()).recordset[0].memes
+  console.log(name);
+  res.status(200).sendFile(__dirname + '\\public\\memes\\' + name)
+})
+
+app.get('/gimmeFilename', async (req, res) => {
+  var name = (await dao.getAllMemes()).recordset[0].memes
+  console.log(name);
+  res.status(200).send('\\memes\\' + name)
+  //console.log('file:///' + __dirname + '\\public\\memes\\' + name);
+})
+
+app.post('/meme', upload.single('uploaded_file'), function (req, res) {
+  // req.file is the name of your file in the form above, here 'uploaded_file'
+  // req.body will hold the text fields, if there were any 
+  console.log(req.file.filename)
+  dao.insertMeme(req.file.filename);
+  res.status(200).send("Successfully uploaded file, go back to see meme inside generator :)")
+});
+
 app.listen(port, () => { console.log("Server listening at http://localhost:" + port) }) // Start the server
