3ba9b066d09e5a71b7c8fcdea2b988232e1b6759
IsadoraTerz <84795870+IsadoraTerz@users.noreply.github.com>
2022-05-16 12:11:30 -0600

    Added another table

--- Link-Aggregator.sql
+++ Link-Aggregator.sql
@@ -13,22 +13,38 @@ CREATE DATABASE linkaggregator;
   USE linkaggregator;
 
 -- Tables
+CREATE TABLE categories( -- All categories
+  id INT IDENTITY(1,1),
+  name VARCHAR(100),
+  color VARCHAR(6),
+  icon VARCHAR(100),
+  PRIMARY KEY(id)
+);
+
 CREATE TABLE links( -- All ratings
   id INT IDENTITY(1,1),
   link VARCHAR(300),
   title VARCHAR(100),
   filename VARCHAR(300),
-  PRIMARY KEY(id)
+  category INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(category) REFERENCES categories(id),
 );
 
-INSERT INTO links VALUES('www.lingscars.com/', 'The UK''s craziest car leasing website!', 'LingsCars');
-INSERT INTO links VALUES('www.tesla.com/', 'Electric Cars, Solar & Clean Energy', 'Tesla');
-INSERT INTO links VALUES('github.com/', 'GitHub', 'GitHub');
+INSERT INTO categories VALUES('Homepage', 'FF6666', 'ic:baseline-10k');
+INSERT INTO categories VALUES('Creative', '66FF66', 'ic:twotone-vpn-lock');
+INSERT INTO categories VALUES('Go Outside', 'FF99FF', 'ic:twotone-wb-sunny');
+
+INSERT INTO links VALUES('www.lingscars.com/', 'The UK''s craziest car leasing website!', 'LingsCars', 1);
+INSERT INTO links VALUES('www.tesla.com/', 'Electric Cars, Solar & Clean Energy', 'Tesla', 2);
+INSERT INTO links VALUES('github.com/', 'GitHub', 'GitHub', 3);
 
 -- ------------------ --
 -- Print out the info --
 -- ------------------ --
 
+SELECT * FROM categories;
 SELECT * FROM links;
 
 --DROP TABLE links;
+--DROP TABLE categories;
