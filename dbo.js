05a1a2a02f875599c2997de35d9e632f645bf2ae
Coderdude112 <47407549+Coderdude112@users.noreply.github.com>
2022-05-16 13:52:32 -0600

    Add queryDatabase back and export correct stuff

--- dbo.js
+++ dbo.js
@@ -4,6 +4,14 @@ const mssql = require("mssql")
 * @param {string} query
 * @returns {string | boolean} The result of running the query or false if the query was unsuccessful
 */
+async function queryDatabase(query) {
+  try {
+    await mssql.connect("Server=localhost,1433; Database=linkaggregator; User Id=link-aggregator-user; Password=password; Encrypt=false;")
+    return await mssql.query(query)
+  } catch (error) {
+    console.warn(error)
+  }
+}
 
 async function createCategory(name, color, icon) {
   return queryDatabase("INSERT INTO categories '" + name + "', '" + color + "', '" + icon + "',")
@@ -22,9 +30,4 @@ async function getLinks() {
 }
 
 /** Handles all exports */
-module.exports = { 
-  //createCategory,
-  //createLink,
-  //getCategories,
-  //getLinks
-}
+module.exports = { createCategory, createLink, getCategories, getLinks }
