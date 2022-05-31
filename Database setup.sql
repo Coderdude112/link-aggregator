f9d5d054712393460e1fceb7b3714785153fa09d
Coderdude112 <47407549+Coderdude112@users.noreply.github.com>
2022-05-31 18:11:49 +0000

    Rename SQL variables to be camelCase

--- Database setup.sql
+++ Database setup.sql
@@ -24,9 +24,9 @@ CREATE TABLE categories( -- All categories
 CREATE TABLE links( -- All ratings
   id INT IDENTITY(1,1),
   link VARCHAR(512),
-  website_name VARCHAR(104),
-  title VARCHAR(104),
-  image_filename VARCHAR(256),
+  websiteName VARCHAR(104),
+  pageTitle VARCHAR(104),
+  imageFilename VARCHAR(256),
   category INT,
   PRIMARY KEY(id),
   FOREIGN KEY(category) REFERENCES categories(id),
@@ -36,13 +36,13 @@ CREATE TABLE links( -- All ratings
 -- Insert data for testing --
 -- ----------------------- --
 
-INSERT INTO categories VALUES('Homepage', 'FF6666', 'ic:baseline-10k');
-INSERT INTO categories VALUES('Creative', '66FF66', 'ic:twotone-vpn-lock');
-INSERT INTO categories VALUES('Go Outside', 'FF99FF', 'ic:twotone-wb-sunny');
+INSERT INTO categories VALUES('General', 'FF6666', 'ic:baseline-inbox');
+INSERT INTO categories VALUES('Security', '66FF66', 'ic:baseline-lock');
+INSERT INTO categories VALUES('Privacy', 'FF99FF', 'ic:baseline-remove-red-eye');
 
-INSERT INTO links VALUES('www.lingscars.com/', 'The UK''s craziest car leasing website!', 'LingsCars', '404.png', 1);
-INSERT INTO links VALUES('www.tesla.com/', 'Electric Cars, Solar & Clean Energy', 'Tesla', '404.png', 2);
-INSERT INTO links VALUES('github.com/', 'GitHub', 'GitHub', '404.png', 3);
+INSERT INTO links VALUES('www.tesla.com', 'Tesla!', 'Electric Cars, Solar & Clean Energy', '', 1);
+INSERT INTO links VALUES('www.freecodecamp.org/news/tag/cybersecurity', 'freeCodeCamp', '#CYBERSECURITY', '', 2);
+INSERT INTO links VALUES('www.freecodecamp.org/news/tag/privacy', 'freeCodeCamp', '#PRIVACY', '', 3);
 
 -- ------------------ --
 -- Print out the info --
